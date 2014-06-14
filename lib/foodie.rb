2c9b69cad94936de286d70603f1816ff0277c89a
josh <jjore@cpan.org>
2014-06-14 09:48:26 -0700

    Add recipe generator

--- lib/foodie.rb
+++ lib/foodie.rb
@@ -1,5 +1,6 @@
 require "foodie/version"
 require 'foodie/food'
+require 'foodie/generators/recipe'
 
 module Foodie
   # Your code goes here...
--- lib/foodie/cli.rb
+++ lib/foodie/cli.rb
@@ -12,5 +12,10 @@ module Foodie
     def pluralize
       puts Foodie::Food.pluralize(options[:word])
     end
+
+    desc "recipe", "Generates a recipe scaffold"
+    def recipe(group, name)
+      Foodie::Generators::Recipe.start([group, name])
+    end
   end
 end
